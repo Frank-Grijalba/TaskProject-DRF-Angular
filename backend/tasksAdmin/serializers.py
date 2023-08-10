3121c0ce22065d4116d575ce0641fa410389a21a
Frank-Grijalba <fjgrijalba@gmail.com>
2023-08-10 12:26:17 -0500

    Api running

--- backend/tasksAdmin/serializers.py
+++ backend/tasksAdmin/serializers.py
@@ -4,5 +4,5 @@ from .models import Task
 class TaskSerializer(serializers.ModelSerializer): # Permite consultar los campos seleccionados de un modelo
     class Meta:
         model = Task
-        fields = ('id', 'title', 'description', 'created', 'task_status', 'task_update_date')
+        fields = ('id', 'title', 'description', 'created', 'status_task', 'update_date_task')
         read_only_fields = ('created', )
