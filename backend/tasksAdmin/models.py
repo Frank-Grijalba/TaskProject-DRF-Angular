61bb899dc6c9821faeb5764602f9468423f6e4b7
Frank Grijalba <77861241+Frank-Grijalba@users.noreply.github.com>
2023-12-13 09:47:01 -0500

    Update models.py

--- backend/tasksAdmin/models.py
+++ backend/tasksAdmin/models.py
@@ -10,6 +10,6 @@ class Task(models.Model):
         ("IN PROGRESS", "IN PROGRESS"),
         ("DONE", "DONE")
     )
-    status_task = models.CharField(max_length=20, choices=TASK_STATUS, blank=True, default= "DONE")
+    status_task = models.CharField(max_length=20, choices=TASK_STATUS, blank=True, default= "TO DO")
     update_date_task = models.DateField(null=True, blank=True)
 
